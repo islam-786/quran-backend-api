d9147699030e1be2924c777ab3deb12fb68a8bca
AxeemHaider <axeemhaider@gmail.com>
2020-06-06 16:41:44 +0500

    Basic doc complete

--- apis/__init__.py
+++ apis/__init__.py
@@ -1,3 +1,4 @@
+from flask import Blueprint
 from flask_restplus import Api
 
 from .edition_api import api as edition_api
@@ -5,11 +6,13 @@ from .surah_api import api as surah_api
 from .translation_api import api as translation_api
 from .ayah_api import api as ayah_api
 
-api = Api(
-    title='My Title',
-    version='1.0',
-    description='A description',
-)
+blueprint = Blueprint('api', __name__)
+api = Api(blueprint,
+          doc='/docs',
+          title='Quran API',
+          version='1.0',
+          description='A description',
+          )
 
 api.add_namespace(edition_api)
 api.add_namespace(surah_api)
--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
-from apis import api
+from apis import blueprint as api
 
 app = Flask(__name__)
-api.init_app(app)
-
+app.register_blueprint(api, url_prefix='/v1')
 app.run(debug=True)
+
+# from flask import Flask
+# from apis import api
+
+# app = Flask(__name__)
+# api.init_app(app)
+
+# app.run(debug=True)
